a893e2ada06e0ebd16147cc93bffafaebd842772
JuancaSterba <sjcexe@gmail.com>
2023-07-30 20:56:51 -0300

    se completa el tercer reto

--- Retos00Inicial.java
+++ Retos00Inicial.java
@@ -3,9 +3,14 @@ import java.util.Arrays;
 public class Retos00Inicial {
     public static void main(String[] args) {
 
-        fizzBuzz(); // Reto # 0
+        // Reto # 0
+        fizzBuzz();
 
-        System.out.println(anagrama("Anaconda","anaconda")); // Reto # 1
+        // Reto # 1
+        System.out.println(anagrama("Anaconda","anaconda"));
+
+        // Reto # 2
+        fibonacci();
 
     }
 
@@ -52,6 +57,26 @@ public class Retos00Inicial {
             return Arrays.equals(letrasPalabra1, letrasPalabra2);
         } else {
             return false;
+        }
+    }
+
+    /**
+     * Reto #2: LA SUCESIÓN DE FIBONACCI
+     * Escribe un programa que imprima los 50 primeros números de la sucesión
+     * de Fibonacci empezando en 0.
+     * - La serie Fibonacci se compone por una sucesión de números en
+     *   la que el siguiente siempre es la suma de los dos anteriores.
+     *   0, 1, 1, 2, 3, 5, 8, 13...
+     */
+
+    public static void fibonacci(){
+        long anterior = 0;
+        long siguiente = 1;
+        for (int i = 0; i < 50; i++){
+            System.out.println(anterior);
+            long suma = anterior + siguiente;
+            anterior = siguiente;
+            siguiente = suma;
         }
     }
 
