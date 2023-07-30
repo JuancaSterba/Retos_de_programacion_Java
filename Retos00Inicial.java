7222b355676c8215e268aa2fae610d9b0acf701d
JuancaSterba <sjcexe@gmail.com>
2023-07-30 20:06:55 -0300

    se completa el segundo reto

--- Retos00Inicial.java
+++ Retos00Inicial.java
@@ -1,4 +1,13 @@
+import java.util.Arrays;
+
 public class Retos00Inicial {
+    public static void main(String[] args) {
+
+        fizzBuzz(); // Reto # 0
+
+        System.out.println(anagrama("Anaconda","anaconda")); // Reto # 1
+
+    }
 
     /**
      * Reto #0: EL FAMOSO "FIZZ BUZZ”
@@ -9,13 +18,6 @@ public class Retos00Inicial {
      * - Múltiplos de 5 por la palabra "buzz".
      * - Múltiplos de 3 y de 5 a la vez por la palabra "fizzbuzz".
      */
-
-    public static void main(String[] args) {
-
-        fizzBuzz();
-
-    }
-
     private static void fizzBuzz() {
         for (int i = 1; i <= 100; i++){
             if (i % 3 == 0 && i % 5 == 0){
@@ -27,6 +29,29 @@ public class Retos00Inicial {
             } else {
                 System.out.println(i);
             }
+        }
+    }
+
+    /**
+     * Reto #1: ¿ES UN ANAGRAMA?
+     * Escribe una función que reciba dos palabras (String) y retorne
+     * verdadero o falso (Bool) según sean o no anagramas.
+     * - Un Anagrama consiste en formar una palabra reordenando TODAS
+     *   las letras de otra palabra inicial.
+     * - NO hace falta comprobar que ambas palabras existan.
+     * - Dos palabras exactamente iguales no son anagrama.
+     */
+    public static boolean anagrama(String palabra1, String palabra2){
+        if (!palabra1.equalsIgnoreCase(palabra2)) {
+            char[] letrasPalabra1 = palabra1.toLowerCase().toCharArray();
+            char[] letrasPalabra2 = palabra2.toLowerCase().toCharArray();
+
+            Arrays.sort(letrasPalabra1);
+            Arrays.sort(letrasPalabra2);
+
+            return Arrays.equals(letrasPalabra1, letrasPalabra2);
+        } else {
+            return false;
         }
     }
 
